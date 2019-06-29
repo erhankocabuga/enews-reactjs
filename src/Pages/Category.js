5e9c4d83ab00163165c4681d50b9a30fba971644
erhan <erhan@DESKTOP-G23NN7J>
2019-06-30 02:15:07 +0300

    Kategori sayfası ve Load More düzenlemesi

--- src/Pages/Category.js
+++ src/Pages/Category.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 
-class Category extends Component {
+import Utils from '../general/utils';
+import NewsService from '../services/NewsService'; 
+
+import NewsList from '../components/NewsList'; 
+
+export default class Category extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newsList: [],
+            pageNo: 1
+        };
+
+        this.params = this.props.match.params; 
+        this.loadMore = this.loadMore.bind(this);
+    };
+
+    getNews(pageNo) {   
+        NewsService
+            .getCategoryNewsSpecific(pageNo, this.params.category)
+            .then((response) => response.json())
+            .then((data) => {
+
+                let news = Utils.mapApiResponseToNewsForList(data);
+                this.setState({
+                    newsList: [
+                        ...this.state.newsList,
+                        ...news
+                    ]
+                }); 
+            })
+            .catch((error) => console.log("Hata:" + error)); 
+    }
+
+    loadMore(pageNo) { 
+        this.getNews(pageNo);
+    }
+
+    componentDidMount() {
+        this.getNews(1);
+    }
+
     render() {
+        const list = this.state.newsList; 
+ 
         return (
-            <div>
-                
-            </div>
+            <Fragment>
+                <NewsList News={list} Type="Wide" LoadMoreAction={this.loadMore} /> 
+            </Fragment>
         );
     }
 }
-
-export default Category;
